f089e51a00e5aa16dea5f41c314ecb253cce8eff
Luong Nguyen Van <s1f101800137@iniad.org>
2019-01-19 12:14:33 +0900

    gg

--- gsite/settings.py
+++ gsite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django'
 ]
 
 MIDDLEWARE = [
@@ -62,13 +63,26 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
+
+
             ],
         },
     },
 ]
+AUTHENTICATION_BACKENDS = (
+ 'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
+ 'social_core.backends.google.GoogleOpenId',  # for Google authentication
+ 'social_core.backends.google.GoogleOAuth2',  # for Google authentication
+ 'social_core.backends.github.GithubOAuth2',  # for Github authentication
+ 'social_core.backends.facebook.FacebookOAuth2',  # for Facebook authentication
 
+ 'django.contrib.auth.backends.ModelBackend',
+)
 WSGI_APPLICATION = 'gsite.wsgi.application'
-
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '84398876133-pghsh4momg442iugcv75lve8trd577mc.apps.googleusercontent.com'  #Paste CLient Key
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'C9BZIHZl_JqFKhEPHfTwXmMo' #Paste Secret Key
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
@@ -118,3 +132,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
--- gsite/urls.py
+++ gsite/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from django.contrib import admin
+from django.contrib.auth import views
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('auth/', include('social_django.urls', namespace='social')),  # <- Here
+    path('', home, name='home'),
 ]
